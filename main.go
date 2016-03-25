b0f80758dfb15f672258750248b810503d3fd1a0
Archer Reilly <1246506786@qq.com>
2016-03-25 14:19:23 +0800

    dynamic get port

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	_ "cola/routers"
 	"github.com/astaxie/beego"
+  "os"
+  "strconv"
 )
 
 func main() {
+  port, err := strconv.Atoi(os.Getenv("PORT"))
+  if err == nil {
+    beego.BConfig.Listen.HTTPPort = port
+  }
 	beego.Run()
 }
 
