ac7951858e2e74e8d81b92b9910f78874512d975
Archer Reilly <1246506786@qq.com>
2016-04-09 15:31:09 +0800

    tidy controller

--- controllers/default.go
+++ controllers/default.go
@@ -9,12 +9,6 @@ type MainController struct {
 	beego.Controller
 }
 
-func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
-}
-
 func (this *MainController) Welcome() {
   var rt models.Result
 
