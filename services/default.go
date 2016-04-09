5d5ddb809a8378dad447e8e2d5b7cc89236d9716
Archer Reilly <1246506786@qq.com>
2016-04-09 16:12:15 +0800

    my local cant connect google

--- services/default.go
+++ services/default.go
@@ -3,12 +3,13 @@ package services
 import (
   "github.com/astaxie/beego"
   "github.com/parnurzeal/gorequest"
+  "time"
 
   "cola/models"
 )
 
 func Read(q string, skip, limit int) (err error, rtvs []models.GoogleResult) {
-  request := gorequest.New()
+  request := gorequest.New().Timeout(5000 * time.Millisecond)
   resp, _, errs := request.Get("https://www.google.com.hk/webhp?hl=en").End()
   if len(errs) != 0 {
     err = errs[0]
